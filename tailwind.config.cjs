df0b77109fdb566c1747309b520aef1376f09c11
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-01 21:50:48 -0300

    :lipstick: add font Rubik

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.tsx", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Rubik', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
