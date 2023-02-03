81b7710d82c4c03b88ec4d058dd2738db6cf0d88
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-02 21:05:01 -0300

    :lipstick: add calculator box-shadow

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './global.css';
 
 function App() {
   return (
-    <div className="bg-[#2D2A37] w-[356px] h-[566px] rounded-[3rem]">
+    <div className="bg-[#2D2A37] w-[356px] h-[566px] rounded-[3rem] shadow-calculatorBg">
 
     </div>
   )
