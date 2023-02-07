d7fe0558cdde29428d032a46c0f9cc8b2afdac5b
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-06 21:25:32 -0300

    :art: add currentValue and operation states and handleKeyClickNumber function

--- src/contexts/calculatorContext.tsx
+++ src/contexts/calculatorContext.tsx
@@ -1,10 +1,12 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 interface CalculatorContextType {
-
+  currentValue: string;
+  operation: string;
+  handleKeyClickNumber: (keyCode: string) => void;
 }
 
-export const CalculatorContext = createContext<CalculatorContextType>({});
+export const CalculatorContext = createContext<CalculatorContextType>({} as CalculatorContextType);
 
 interface CalculatorContextProviderProps {
   children: React.ReactNode;
@@ -12,8 +14,22 @@ interface CalculatorContextProviderProps {
 
 
 export function CalculatorContextProvider({children}: CalculatorContextProviderProps) {
+  const [currentValue, setCurrentValue] = useState('');
+  const [operation, setOperation] = useState('...');
+
+  const handleKeyClickNumber = (keyCode: string) => { //De 0 a 9
+    console.log(keyCode)
+    // if(['ce', 'c', '%', ',', '+-'].includes(keyCode)) {
+    //   return;
+    // }
+    // if(['+', '-', 'x', '/'].includes(keyCode)) {
+    //   return;
+    // }
+    setOperation(operation.length < 1 ? keyCode : `${operation} ${keyCode}`);
+  }
+
   return (
-    <CalculatorContext.Provider value={{}}>
+    <CalculatorContext.Provider value={{ currentValue, operation, handleKeyClickNumber }}>
       {children}
     </CalculatorContext.Provider>
   )
