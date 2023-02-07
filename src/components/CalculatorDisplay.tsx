09ab7d3b7bdef9f80f8cb176cb2ac13152074581
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-06 21:24:49 -0300

    :art: update CalculatorDisplay.tsx

--- src/components/CalculatorDisplay.tsx
+++ src/components/CalculatorDisplay.tsx
@@ -1,18 +1,17 @@
 import { Equals } from "phosphor-react";
-import { useState } from "react";
+import { useCalculator } from "../hooks/useCalculator";
 
 export function CalculatorDisplay() {
-  const [result, setResult] = useState('...');
-  const [operations, setOperations] = useState('...');
+  const { operation } = useCalculator();
 
   return (
     <div className="head-calculator">
-        <div className="head-calculator-operations">{operations}</div>
+        <div className="head-calculator-operations">{operation}</div>
         <div className="head-calculator-result">
           <span>
             <Equals size={24} color="#6B6B6B" />
           </span>
-          <span className="text-[2rem]">{result}</span>
+          <span className="text-[2rem]">2</span>
         </div>
       </div>
   )
