09c194020789d1bf330dcae41b3fb986adbff67b
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-10 21:52:13 -0300

    :white_check_mark: add subtraction test

--- cypress/e2e/calculator-tests/operations.cy.js
+++ cypress/e2e/calculator-tests/operations.cy.js
@@ -3,12 +3,21 @@ describe('Use the calculator', () => {
     cy.visit('http://localhost:5173')
   })
 
-  it('sum to numbers', () => {
+  it('sum two numbers', () => {
     cy.get('#key-2').click();
     cy.get('#key-plus').click();
     cy.get('#key-2').click();
     cy.get('#key-equals').click();
 
     cy.get('#result').should('have.text', '4');
+  })
+
+  it('subtraction two numbers', () => {
+    cy.get('#key-6').click();
+    cy.get('#key-minus').click();
+    cy.get('#key-4').click();
+    cy.get('#key-equals').click();
+
+    cy.get('#result').should('have.text', '2');
   })
 })
