fd24ad0edf21c718909d92e15e6f7b7530a3ace8
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-10 21:49:40 -0300

    :art: add id to keys

--- src/components/CalculatorKeyboard.tsx
+++ src/components/CalculatorKeyboard.tsx
@@ -16,30 +16,30 @@ export function CalculatorKeyboard() {
 
   return (
     <div className="body-calculator">
-      <Key code="ce" keyName="CE" color="violet" onKeyClick={handleClearCurrentKeyPressed} />
-      <Key code="c" keyName="C" onKeyClick={handleClearAll} />
-      <Key code="%" children={<Percent size={28} />} onKeyClick={handleKeyClickPercent} />
-      <Key code="/" children={<Divide size={28} />} backgroundColor="2" onKeyClick={handleKeyClickOperation} />
+      <Key id="key-ce" code="ce" keyName="CE" color="violet" onKeyClick={handleClearCurrentKeyPressed} />
+      <Key id="key-c" code="c" keyName="C" onKeyClick={handleClearAll} />
+      <Key id="key-percent" code="%" children={<Percent size={28} />} onKeyClick={handleKeyClickPercent} />
+      <Key id="key-divide" code="/" children={<Divide size={28} />} backgroundColor="2" onKeyClick={handleKeyClickOperation} />
 
-      <Key code="7" keyName="7" onKeyClick={handleKeyClickNumber} />
-      <Key code="8" keyName="8" onKeyClick={handleKeyClickNumber}  />
-      <Key code="9" keyName="9" onKeyClick={handleKeyClickNumber} />
-      <Key code="x" children={<X size={28} />} backgroundColor="2" onKeyClick={handleKeyClickOperation} />
+      <Key id="key-7" code="7" keyName="7" onKeyClick={handleKeyClickNumber} />
+      <Key id="key-8" code="8" keyName="8" onKeyClick={handleKeyClickNumber}  />
+      <Key id="key-9" code="9" keyName="9" onKeyClick={handleKeyClickNumber} />
+      <Key id="key-multiplication" code="x" children={<X size={28} />} backgroundColor="2" onKeyClick={handleKeyClickOperation} />
 
-      <Key code="4" keyName="4" onKeyClick={handleKeyClickNumber} />
-      <Key code="5" keyName="5" onKeyClick={handleKeyClickNumber} />
-      <Key code="6" keyName="6" onKeyClick={handleKeyClickNumber} />
-      <Key code="-" children={<Minus size={28} />} backgroundColor="2" onKeyClick={handleKeyClickOperation} />
+      <Key id="key-4" code="4" keyName="4" onKeyClick={handleKeyClickNumber} />
+      <Key id="key-5" code="5" keyName="5" onKeyClick={handleKeyClickNumber} />
+      <Key id="key-6" code="6" keyName="6" onKeyClick={handleKeyClickNumber} />
+      <Key id="key-minus" code="-" children={<Minus size={28} />} backgroundColor="2" onKeyClick={handleKeyClickOperation} />
 
-      <Key code="1" keyName="1" onKeyClick={handleKeyClickNumber} />
-      <Key code="2" keyName="2" onKeyClick={handleKeyClickNumber} />
-      <Key code="3" keyName="3" onKeyClick={handleKeyClickNumber} />
-      <Key code="+" children={<Plus size={28} />} backgroundColor="2" onKeyClick={handleKeyClickOperation} />
+      <Key id="key-1" code="1" keyName="1" onKeyClick={handleKeyClickNumber} />
+      <Key id="key-2" code="2" keyName="2" onKeyClick={handleKeyClickNumber} />
+      <Key id="key-3" code="3" keyName="3" onKeyClick={handleKeyClickNumber} />
+      <Key id="key-plus" code="+" children={<Plus size={28} />} backgroundColor="2" onKeyClick={handleKeyClickOperation} />
 
-      <Key code="+-" children={<PlusMinus size={28} />} backgroundColor="2" onKeyClick={handleKeyClickPlusMinus} />
-      <Key code="0" keyName="0" onKeyClick={handleKeyClickNumber} />
-      <Key code="." keyName="." onKeyClick={handleKeyClickAddDot} />
-      <Key code="=" children={<Equals size={28} />} backgroundColor="3" onKeyClick={handleResultOperation} />
+      <Key id="key-plusminus" code="+-" children={<PlusMinus size={28} />} backgroundColor="2" onKeyClick={handleKeyClickPlusMinus} />
+      <Key id="key-0" code="0" keyName="0" onKeyClick={handleKeyClickNumber} />
+      <Key id="key-dot" code="." keyName="." onKeyClick={handleKeyClickAddDot} />
+      <Key id="key-equals" code="=" children={<Equals size={28} />} backgroundColor="3" onKeyClick={handleResultOperation} />
     </div>
   )
 }
--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export interface KeyProps {
+  id: string;
   code: '0'| '1'| '2'| '3'| '4'| '5'| '6'| '7'| '8'| '9'|
         'ce'| 'c'| '%'| '/'| 'x'| '-'| '+'| '='| '+-'| '.';
   keyName?: string;
@@ -11,6 +12,7 @@ export interface KeyProps {
 }
 
 export function Key({
+  id,
   code,
   keyName,
   children,
@@ -20,6 +22,7 @@ export function Key({
 }: KeyProps) {
   return (
     <div
+      id={id}
       onClick={() => onKeyClick(code)}
       className={
         `body-calculator-key
